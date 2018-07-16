e95cdc7b98aac310b28037e235ecb50af5002a28
Kanika <kanikachander1@gmail.com>
2018-07-16 23:43:34 +0530

    Updated with async method

--- aggregate.js
+++ aggregate.js
@@ -4,90 +4,57 @@
  */
 const fs = require('fs');
 
-const continentList = ['South America', 'Oceania', 'North America', 'Asia', 'Europe', 'Africa'];
+const mapPath = 'data/country-mapper-file.json';
 
-const asiaCountries = ['"China"', '"India"', '"Indonesia"', '"Japan"', '"Russia"', '"Saudi Arabia"', '"Turkey"', '"Republic of Korea"'];
-const southAmericaCountries = ['"Argentina"', '"Brazil"'];
-const oceaniaCountries = ['"Australia"'];
-const northAmericaCountries = ['"Canada"', '"Mexico"', '"USA"'];
-const europeCountries = ['"France"', '"Germany"', '"Italy"', '"United Kingdom"'];
-const africaCountries = ['"South Africa"'];
-
-const output = {
-  'South America': { GDP_2012: 0, POPULATION_2012: 0 },
-  Oceania: { GDP_2012: 0, POPULATION_2012: 0 },
-  'North America': { GDP_2012: 0, POPULATION_2012: 0 },
-  Asia: { GDP_2012: 0, POPULATION_2012: 0 },
-  Europe: { GDP_2012: 0, POPULATION_2012: 0 },
-  Africa: { GDP_2012: 0, POPULATION_2012: 0 },
+const readFile = (file) => {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+  return promise;
 };
 
-function csvToArray(csv) {
-  const rows = csv.split(['\n']);
-
-  return rows.map(row => row.split(','));
-  // return rows.map(function (row) {
-  //   return row.split(',');
-  // });
-}
-
-function arrToObject(arr) {
-  // assuming header
-  const keys = arr[0];
-  // vacate keys from main array
-  const newArr = arr.slice(1, arr.length);
-
-  const formatted = [];
-  const l = keys.length;
-  for (let i = 0; i < newArr.length; i += 1) {
-    const d = newArr[i];
-    const o = {};
-    for (let j = 0; j < l; j += 1) {
-      o[keys[j]] = d[j];
-    }
-    formatted.push(o);
-  }
-  return formatted;
-}
+const writeFile = (file, content) => {
+  const promise = new Promise((resolve, reject) => {
+    fs.writeFile(file, content, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+  return promise;
+};
 
 const aggregate = (filePath) => {
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+  Promise.all([readFile(filePath), readFile(mapPath)]).then((values) => {
+    const csvFileData = (values[0].replace(/"/g, '')).split('\n');
+    const mapFileData = JSON.parse(values[1]);
+    const headerTxt = csvFileData[0].split(',');
+    const rowsTxt = csvFileData.slice(1, csvFileData.length);
+    const indexCountryName = headerTxt.indexOf('Country Name');
+    const indexGDP2012 = headerTxt.indexOf('GDP Billions (US Dollar) - 2012');
+    const indexPopulation2012 = headerTxt.indexOf('Population (Millions) - 2012');
 
-  const fileContentArray = csvToArray(fileContent);
-  const fileContentJSONObj = arrToObject(fileContentArray);
-
-  function continent(countryArray) {
-    let GDP = 0;
-    let population = 0;
-    for (let i = 0; i < (fileContentJSONObj.length); i += 1) {
-      for (let j = 0; j < countryArray.length; j += 1) {
-        if (countryArray[j] === fileContentJSONObj[i]['"Country Name"']) {
-          GDP += parseFloat((fileContentJSONObj[i]['"GDP Billions (US Dollar) - 2012"']).replace('"', ''));
-          population += parseFloat((fileContentJSONObj[i]['"Population (Millions) - 2012"']).replace('"', ''));
+    const output = {};
+    rowsTxt.forEach((value) => {
+      const rowCells = value.split(',');
+      const continentName = mapFileData[rowCells[indexCountryName]];
+      if (continentName !== undefined && rowCells[indexCountryName] !== '' && continentName !== undefined) {
+        if (output[continentName] === undefined) {
+          output[continentName] = {};
+          output[continentName].GDP_2012 = parseFloat(rowCells[indexGDP2012]);
+          output[continentName].POPULATION_2012 = parseFloat(rowCells[indexPopulation2012]);
+        } else {
+          output[continentName].GDP_2012 += parseFloat(rowCells[indexGDP2012]);
+          output[continentName].POPULATION_2012 += parseFloat(rowCells[indexPopulation2012]);
         }
       }
-    }
-    return [GDP, population];
-  }
-
-  const mapFile = [continent(southAmericaCountries),
-    continent(oceaniaCountries),
-    continent(northAmericaCountries),
-    continent(asiaCountries),
-    continent(europeCountries),
-    continent(africaCountries)];
-
-  // Assigning the GDP_2012 in the output file
-  for (let i = 0; i < continentList.length; i += 1) {
-    const variable1 = mapFile[i][0];
-    output[continentList[i]].GDP_2012 = variable1;
-  }
-  // Assigning the POPULATION_2012 in the output file
-  for (let i = 0; i < continentList.length; i += 1) {
-    const variable2 = mapFile[i][1];
-    output[continentList[i]].POPULATION_2012 = variable2;
-  }
-  fs.writeFileSync('output/output.json', JSON.stringify(output));
+    });
+    writeFile('output/output.json', JSON.stringify(output));
+  });
 };
+
+// aggregate('data/datafile.csv');
 
 module.exports = aggregate;
